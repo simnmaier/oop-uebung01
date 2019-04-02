6af2a4ee23c52948961a1e291a205bb686c37726
simonmaier <simon.maier_1@stud.fh-rosenheim.de>
2019-04-02 14:31:40 +0200

    implemented CharStackImpl Class

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -1,8 +1,25 @@
 package ueb01;
 
-class Palindrom {
+class Palindrom{
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
+
+		if (s.length() == 1){
+			return true;
+		}
+
+		CharStack stack = new CharStackImpl();
+		char [] cs = s.toLowerCase().replaceAll("","").toCharArray();
+
+		int i =0;
+		for (;i < cs.length / 2; i++){
+			stack.push(cs[i]);
+		}
+
+		if (cs.length % 2 ==1){
+			i++;
+		}
+
 
 		return false;
 	}
